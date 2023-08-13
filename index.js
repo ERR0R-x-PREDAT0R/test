9b202a2059f15fa65cae8ca49c62e33783d5b5b9
ALINSHAN <92071951+ERR0R-x-PREDAT0R@users.noreply.github.com>
2023-08-13 20:33:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('ABHISHEK-SER', {
+say('jarvis', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @ABHISHEK SURESH._`, {
+say(`'${name}' By @Alinshan._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
